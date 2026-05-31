42027da6d230c523c450e1680c975585f15e7b2d
leognobrega <leo.gnobrega@hotmail.com>
2026-05-31 09:56:00 -0300

    Arrumando ID do model COMPRA

--- src/main/java/com/easygest/springboot/model/Compra.java
+++ src/main/java/com/easygest/springboot/model/Compra.java
@@ -2,13 +2,13 @@ package com.easygest.springboot.model;
 
 import java.time.LocalDate;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
